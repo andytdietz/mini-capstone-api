01bc20e7900bd794f52907a5fc51f2e2b0856694
andytdietz <andytdietz@gmail.com>
2024-02-27 15:07:03 -0700

    add find by id

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
   get "/first_product", controller: "products", action: "first_product"
   get "/all_products", controller: "products", action: "all_products"
   get "/tortilla_press", controller: "products", action: "tortilla_press"
-  get "tiny_cowboy_hat", controller: "products", action: "tiny_cowboy_hat"
+  get "/tiny_cowboy_hat", controller: "products", action: "tiny_cowboy_hat"
   get "/product/:id", controller: "products", action: "product_id"
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
