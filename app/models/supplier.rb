5fc79f2c5a3fd45baf3caefc60fccbeb87ae731e
andytdietz <andytdietz@gmail.com>
2024-03-07 14:41:43 -0700

    add products in supplier view

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -4,4 +4,8 @@ class Supplier < ApplicationRecord
   validates :email, presence: true
   validates :phone_number, presence: true
   has_many :products
+
+  def images
+    Product.where(supplier_id: id)
+  end
 end
