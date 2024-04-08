5b30fcc2c9401a793b7b6cc8f3366363308643ae
andytdietz <andytdietz@gmail.com>
2024-04-07 20:27:25 -0600

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ Supplier.create!([
   { name: "Shark", email: "help@shark.com", phone_number: "7654321" },
 ])
 Product.create!([
-  { name: "Shark Vacuum", price: "200.0", description: "A vacuum that is bloodthirsty", inventory: nil, supplier_id: 2 },
-  { name: "Silonn Ice Maker", price: "89.0", description: "Fast ice maker!", inventory: nil, supplier_id: 1 },
-  { name: "8 Inch Tortilla Press", price: "12.0", description: "Make your own homemade tortillas with this cast aluminum tortilla press!", inventory: nil, supplier_id: 1 },
+  { name: "Shark Vacuum", price: "200.00", description: "A vacuum that is bloodthirsty", inventory: nil, supplier_id: 2 },
+  { name: "Silonn Ice Maker", price: "89.00", description: "Fast ice maker!", inventory: nil, supplier_id: 1 },
+  { name: "8 Inch Tortilla Press", price: "12.00", description: "Make your own homemade tortillas with this cast aluminum tortilla press!", inventory: nil, supplier_id: 1 },
   { name: "Computer Monitor", price: "150.0", description: "24 inch full HD IPS monitor with thin bezels and 100Hz refresh rate", inventory: nil, supplier_id: 1 },
-  { name: "The Rock Blanket", price: "20.0", description: "A very fluffy blanket with Dwayne The Rock Johnson's face on it. Adding more to make this longer", inventory: nil, supplier_id: 1 },
-  { name: "cat toy", price: "4.0", description: "toy for your cat. your cat will absolultely love this toy, or maybe not.", inventory: nil, supplier_id: 1 },
+  { name: "The Rock Blanket", price: "20.00", description: "A very fluffy blanket with Dwayne The Rock Johnson's face on it. Adding more to make this longer", inventory: nil, supplier_id: 1 },
+  { name: "cat toy", price: "4.00", description: "toy for your cat. your cat will absolultely love this toy, or maybe not.", inventory: nil, supplier_id: 1 },
 ])
 Image.create!([
   { url: "https://m.media-amazon.com/images/I/61eMsTk4w0L._SX342_.jpg", product_id: 2 },
